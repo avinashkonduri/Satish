8b01720f07293c17ecb82b91879af9a74de64fc6
avinashk <avinashk@logicmatter.com>
2021-02-23 00:43:13 +0530

    Create Buttons from an array and get testcases based on click

--- src/app/ui-degitestbuddy/ui-degitestbuddy.component.ts
+++ src/app/ui-degitestbuddy/ui-degitestbuddy.component.ts
@@ -8,12 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class UiDegitestbuddyComponent implements OnInit {
   testPlatforms!: string[];
   selectedPlatform!: string;
-
+  big6: string[] = [];
+  testButtons: string[] = ['Test1', 'Test2', 'Test3', 'Test4']
   constructor() { }
 
   ngOnInit(): void {
     this.testPlatforms = ["Desktop", "Mobile"];
     this.selectedPlatform = this.testPlatforms[0];
+    //this.myBig6();
   }
 
+  myBig6() {
+    this.big6 = ['Load Website', 'Tracking', 'Login to casino', 'Load Website1', 'Tracking1', 'Login to casino1']
+  }
+
+  testCase1() {
+    this.big6 = ['Load Website', 'Tracking', 'Login to casino', 'Load Website1', 'Tracking1', 'Login to casino1']
+  }
+  getTestCases(evt: any) {
+    console.log(evt.target.value);
+    switch (evt.target.value) {
+      case 'Test1':
+        this.big6 = ['Load Website', 'Tracking', 'Login to casino', 'Load Website1', 'Tracking1', 'Login to casino1'];
+        break;
+      case 'Test2':
+        this.big6 = ['Load Website2', 'Tracking2', 'Login to casino2', 'Load Website12', 'Tracking12', 'Login to casino12'];
+        break;
+      case 'Test3':
+        this.big6 = ['Load Website3', 'Tracking3', 'Login to casino3', 'Load Website13', 'Tracking13', 'Login to casino13'];
+        break;
+      case 'Test4':
+        this.big6 = ['Load Website4', 'Tracking4', 'Login to casino4', 'Load Website14', 'Tracking14', 'Login to casino14'];
+        break;
+    }
+  }
 }
